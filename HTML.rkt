24c5c62f82a17c86fb2e306e7b61999cff5cf0c6
Michael Tighe Haag <michaeltighehaag@gmail.com>
2019-03-06 05:06:52 -0500

    add more html functions

--- HTML.rkt
+++ HTML.rkt
@@ -20,11 +20,16 @@
 [define [html:title al el] [cons 'title [cons [cons '@ al] el]]]
 [define [html:link al el] [cons 'link [cons [cons '@ al] el]]]
 [define [html:script al el] [cons 'script [cons [cons '@ al] el]]]
+[define [html:style al el] [cons 'style [cons [cons '@ al] el]]]
 [define [html:body al el] [cons 'body [cons [cons '@ al] el]]]
 [define [html:p al el] [cons 'p [cons [cons '@ al] el]]]
 [define [html:div al el] [cons 'div [cons [cons '@ al] el]]]
 [define [html:span al el] [cons 'span [cons [cons '@ al] el]]]
 [define [html:h3 al el] [cons 'h3 [cons [cons '@ al] el]]]
+[define [html:table al el] [cons 'table [cons [cons '@ al] el]]]
+[define [html:tr al el] [cons 'tr [cons [cons '@ al] el]]]
+[define [html:td al el] [cons 'td [cons [cons '@ al] el]]]
+[define [html:a al el] [cons 'a [cons [cons '@ al] el]]]
 
 
 [define [mk-html html-attr-list head-list body-list]
