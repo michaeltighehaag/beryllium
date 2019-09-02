6cd6f5f38825fb87846866c212bf5f40e37d48a9
Michael Tighe Haag <michaeltighehaag@gmail.com>
2019-09-02 07:22:40 -0400

    Added more work in process code for turning trees into headers for css based div tables.

--- HTML.rkt
+++ HTML.rkt
@@ -81,49 +81,54 @@
 "}"
 ]]
       
-[define [mk-table-div-id-css l] [stream
-  [cat "#cell" [list-ref l 0] "-" [list-ref l 1] " {"]
-  [cat "  grid-area: " [list-ref l 0] " / " [list-ref l 1] " / span 1 / span 1;"] 
+[define [mk-table-div-id-css p l] [stream
+  [cat "#cell-" p "-" [list-ref l 1] "-" [list-ref l 3] " {"]
+  [cat "  grid-area: " [list-ref l 1] " / " [list-ref l 3] " / span " [list-ref l 2] " / span " [list-ref l 4] ";"] 
   [cat "}"]]]
 
-[define [mk-table-div-element l]
-  [html:div [list [cons 'id [cat "cell" [list-ref l 0] "-" [list-ref l 1]]]]
-            [list [list-ref l 2]]]]
+[define [mk-table-div-element p l]
+  [html:div [list [cons 'id [cat "cell-" p "-" [list-ref l 1] "-" [list-ref l 3]]]]
+            [list [list-ref l 0]]]]
 
 
 [define [mk-table-stream df row-start row-strm col-start col-strm]
-  [let [[nrs [stream-zip [stream-map [lambda [x] [number->string x]] [stream-from row-start]] row-strm]]
-        [ncs [stream-zip [stream-map [lambda [x] [number->string x]] [stream-from col-start]] col-strm]]]
+  [let [[nrs [stream-zip row-strm [stream-map [lambda [x] [number->string x]] [stream-from row-start]]]]
+        [ncs [stream-zip col-strm [stream-map [lambda [x] [number->string x]] [stream-from col-start]]]]]
   [stream-concat
   [stream-map
     [lambda [x]
       [stream-map
-        [lambda [y] [list [car x] [car y] [df [cadr x] [cadr y]] ]]
+        [lambda [y]  [df [cadr x] [cadr y] [car x] [car y]] ]
         nrs]]
     ncs]]]]
 
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-[define [test-css d] 
+[define [test-css dl] 
   [stream-append
     diff-css
     table-div-css
-    [stream-concat [stream-map mk-table-div-id-css d]] ]]
+    [stream-concat [stream-map [lambda [x] [mk-table-div-id-css "t1" x]] [car dl]] ]
+    [stream-concat [stream-map [lambda [x] [mk-table-div-id-css "t2" x]] [cadr dl]]]
+    ]]
  
-[define [test-divs d]
+[define [test-divs p d]
   [list<-stream
-  [stream-map mk-table-div-element d]]]
+  [stream-map [lambda [x] [mk-table-div-element p x]] d]]]
 
-[define [test-df x y] [list x y
-                             [cat "h" x "v" y "?"]]]
+[define [test-df x y xh yh] [list 
+                             [cat "h" xh "v" yh "?"] x "1" y "1"]]
 
 [define test-rs [stream-map [lambda [x] [pad x 2]] [stream-range 21 30]]]
-[define test-cs [stream-map [lambda [x] [pad x 2]] [stream-range 30 41]]]
+[define test-cs [stream-map [lambda [x] [pad x 2]] [stream-range 70 81]]]
 
-[define [html-table-test]                                            
-  [let* [[ts [mk-table-stream test-df  1 test-rs 1 test-cs]]
-         [name "html-table-test"]
+[define [html-table-test1]                                            
+  [let* [[ts1 [mk-table-stream test-df  1 test-rs 1 test-cs]]
+         [ts2 [stream<-list [map [lambda [x] [cons [car x] [map number->string [cdr x]]]]
+                                 [walk-header-tree rl 0]]
+                           ]]
+         [name "html-table-test1"]
          [out-file-html
            [open-output-file [cat "../" name ".html"] #:exists 'replace]]]
     [write-html
@@ -139,52 +144,26 @@
                 [html:span [list [list 'class "diff-ins"]] [list "ing"]]]]]
           [html:div
             [list [cons 'class "grid-container"]]
-            [test-divs ts]]]] out-file-html]
-    [close-output-port out-file-html]
-    [file<-stream [cat "../" name ".css"] displayln [test-css ts]]
-
-    ]]
-
-
-
-[define [html-table-test2]                                            
-  [let* [[ts2 [stream [list "1" "3" "head"]  [list "2" "3" "head2"]
-                      [list "3" "1" "rh"][list "3" "2" "rh2"]]]
-         [ts1 [mk-table-stream test-df  3 test-rs 3 test-cs]]
-         [ts [stream-append ts2 ts1 ]]
-         [name "html-table-test2"]
-         [out-file-html
-           [open-output-file [cat "../" name ".html"] #:exists 'replace]]]
-    [write-html
-      [mk-html
-        [list]
-        [list]
-        [list [mk-css-link [cat "./" name ".css"]]]
-        [list]
-        [list
-          [list [html:p [list] [list
-                [html:span [list [list 'class "diff-mod"]] [list "T"]]
-                "est"
-                [html:span [list [list 'class "diff-ins"]] [list "ing"]]]]]
+            [test-divs "t1" ts1]]
+          [list [html:p [list] [list "est"]]]
           [html:div
             [list [cons 'class "grid-container"]]
-            [test-divs ts]]]] out-file-html]
+            [test-divs "t2" ts2]]
+          ]]
+      out-file-html]
     [close-output-port out-file-html]
-    [file<-stream [cat "../" name ".css"] displayln [test-css ts]]
+    [file<-stream [cat "../" name ".css"] displayln [test-css [list ts1 ts2]]]
 
     ]]
 
-[define cl [list "a"
-                 [list "s1" [list "x1"] [list "x2"] [list "x3"]]
-                 [list "s2" [list "y1"] [list "y2" [list "z1"]] [list "y3"]]]]
+
 [define rl [list "a"
                  [list "s1" [list "x1"] [list "x2"] [list "x3"]]
                  [list "s2" [list "y1"] [list "y2" [list "z1"][list "z2"]] [list "y3"]]]]
 
-[define [rmk-cell i l]
-  [if [not [null? l]]
-    [displayln [list i [leaf-count [car l]] [caar l]]]
-    [rmk-cell [+ i [leaf-count l]] [cdr l]]]]
+
+[define [walk-header-tree t [p 0]]
+  [wht-h t [list] 1 1 [add1  [tree-depth t]] p]]
 
 [define [leaf-count t]
   [if [null? [cdr t]] 1
@@ -193,19 +172,6 @@
 [define [tree-depth t]
   [if [null? [cdr t]] 1
     [+ 1 [apply max [map tree-depth [cdr t]]]]]]
-    
-[define [walk-level t lev]
-  [if [equal? 1 lev] [list [car t] lev [leaf-count t]]
-    [apply list [map [lambda [x] [walk-level x [sub1 lev]]] [cdr t]]]]] 
-
-[define [nw t r cd td]
-  [if [null? [cdr t]] [cons [list [car t] cd [- td [tree-depth t]]] r]
-    [void]]
-]
-
-
-[define [walk-header-tree t [p 0]]
-  [wht-h t [list] 1 1 [add1  [tree-depth t]] p]]
 
 [define [wht-h t r lev i d p]
   [let* [[slev [if [equal? p 1] 1 [- d [+ [sub1 lev] [tree-depth t]]]]]
@@ -216,39 +182,5 @@
   [let [[n [wht-h [car l] r lev i d p]]]
     [if [null? [cdr l]] n [wht-l [cdr l] n lev [+ i [leaf-count [car l]]] d p]]]]
 
-[displayln [walk-header-tree rl]]
-           
-[displayln [walk-header-tree rl 1]]
-       
-[walk-level rl 1]
-[walk-level rl 2]
-[walk-level rl 3]
-[walk-level rl 4]
-[define [html-table-test3]                                            
-  [let* [[ts2 [stream<-list [map [lambda [x] [list [number->string [cadr x]]
-                                                   [number->string [cadddr x]][car x]]] [walk-header-tree rl 0]]
-                           ]]
-         [ts1 [mk-table-stream test-df  3 test-rs 3 test-cs]]
-         [ts [stream-append ts2 ]]
-         [name "html-table-test4"]
-         [out-file-html
-           [open-output-file [cat "../" name ".html"] #:exists 'replace]]]
-    [write-html
-      [mk-html
-        [list]
-        [list]
-        [list [mk-css-link [cat "./" name ".css"]]]
-        [list]
-        [list
-          [list [html:p [list] [list
-                [html:span [list [list 'class "diff-mod"]] [list "T"]]
-                "est"
-                [html:span [list [list 'class "diff-ins"]] [list "ing"]]]]]
-          [html:div
-            [list [cons 'class "grid-container"]]
-            [test-divs ts]]]] out-file-html]
-    [close-output-port out-file-html]
-    [file<-stream [cat "../" name ".css"] displayln [test-css ts]]
 
-    ]]
-[html-table-test3]
+[html-table-test1]
