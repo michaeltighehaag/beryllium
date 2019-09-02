3ec151095599692a5018e2c2d09cc70d067b1ac6
Michael Tighe Haag <michaeltighehaag@gmail.com>
2019-09-02 09:29:49 -0400

    Added even more work in process code for turning trees into headers for css based div tables.

--- HTML.rkt
+++ HTML.rkt
@@ -30,6 +30,7 @@
 [define [html:tr al el] [cons 'tr [cons [cons '@ al] el]]]
 [define [html:td al el] [cons 'td [cons [cons '@ al] el]]]
 [define [html:a al el] [cons 'a [cons [cons '@ al] el]]]
+[define [html:br al el] [cons 'br [cons [cons '@ al] el]]]
 
 
 [define [mk-html html-attr-list head-attr-list head-list body-attr-list body-list]
@@ -37,8 +38,6 @@
       [html:head head-attr-list head-list]
       [html:body body-attr-list body-list]
   ]]]
-
-
 
 [define [mk-css-link loc]
   [html:link
@@ -81,85 +80,36 @@
 "}"
 ]]
       
-[define [mk-table-div-id-css p l] [stream
-  [cat "#cell-" p "-" [list-ref l 1] "-" [list-ref l 3] " {"]
-  [cat "  grid-area: " [list-ref l 1] " / " [list-ref l 3] " / span " [list-ref l 2] " / span " [list-ref l 4] ";"] 
+[define [mk-table-div-id-css l] [stream
+  [cat "#cell-" [list-ref l 1]  "-" [list-ref l 2] "-" [list-ref l 4] " {"]
+  [cat "  grid-area: " [list-ref l 2] " / " [list-ref l 4] " / span " [list-ref l 3] " / span " [list-ref l 5] ";"] 
   [cat "}"]]]
 
-[define [mk-table-div-element p l]
-  [html:div [list [cons 'id [cat "cell-" p "-" [list-ref l 1] "-" [list-ref l 3]]]]
+[define [mk-table-div-element l]
+  [html:div [list [cons 'id [cat "cell-" [list-ref l 1]  "-" [list-ref l 2] "-" [list-ref l 4]]]]
             [list [list-ref l 0]]]]
 
+[define [mk-table-css dss] 
+  [stream-concat
+  [stream-map [lambda [x] [stream-concat [stream-map mk-table-div-id-css x] ]]
+  dss]]]            
 
-[define [mk-table-stream df row-start row-strm col-start col-strm]
+[define [mk-table-divs ds]
+  [html:div
+    [list [cons 'class "grid-container"]]
+    [list<-stream [stream-map mk-table-div-element ds]]]]
+
+
+[define [mk-table-stream-block df p row-start row-strm col-start col-strm]
   [let [[nrs [stream-zip row-strm [stream-map [lambda [x] [number->string x]] [stream-from row-start]]]]
         [ncs [stream-zip col-strm [stream-map [lambda [x] [number->string x]] [stream-from col-start]]]]]
   [stream-concat
   [stream-map
     [lambda [x]
       [stream-map
-        [lambda [y]  [df [cadr x] [cadr y] [car x] [car y]] ]
+        [lambda [y]  [df p [cadr x] [cadr y] [car x] [car y]] ]
         nrs]]
     ncs]]]]
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-[define [test-css dl] 
-  [stream-append
-    diff-css
-    table-div-css
-    [stream-concat [stream-map [lambda [x] [mk-table-div-id-css "t1" x]] [car dl]] ]
-    [stream-concat [stream-map [lambda [x] [mk-table-div-id-css "t2" x]] [cadr dl]]]
-    ]]
- 
-[define [test-divs p d]
-  [list<-stream
-  [stream-map [lambda [x] [mk-table-div-element p x]] d]]]
-
-[define [test-df x y xh yh] [list 
-                             [cat "h" xh "v" yh "?"] x "1" y "1"]]
-
-[define test-rs [stream-map [lambda [x] [pad x 2]] [stream-range 21 30]]]
-[define test-cs [stream-map [lambda [x] [pad x 2]] [stream-range 70 81]]]
-
-[define [html-table-test1]                                            
-  [let* [[ts1 [mk-table-stream test-df  1 test-rs 1 test-cs]]
-         [ts2 [stream<-list [map [lambda [x] [cons [car x] [map number->string [cdr x]]]]
-                                 [walk-header-tree rl 0]]
-                           ]]
-         [name "html-table-test1"]
-         [out-file-html
-           [open-output-file [cat "../" name ".html"] #:exists 'replace]]]
-    [write-html
-      [mk-html
-        [list]
-        [list]
-        [list [mk-css-link [cat "./" name ".css"]]]
-        [list]
-        [list
-          [list [html:p [list] [list
-                [html:span [list [list 'class "diff-mod"]] [list "T"]]
-                "est"
-                [html:span [list [list 'class "diff-ins"]] [list "ing"]]]]]
-          [html:div
-            [list [cons 'class "grid-container"]]
-            [test-divs "t1" ts1]]
-          [list [html:p [list] [list "est"]]]
-          [html:div
-            [list [cons 'class "grid-container"]]
-            [test-divs "t2" ts2]]
-          ]]
-      out-file-html]
-    [close-output-port out-file-html]
-    [file<-stream [cat "../" name ".css"] displayln [test-css [list ts1 ts2]]]
-
-    ]]
-
-
-[define rl [list "a"
-                 [list "s1" [list "x1"] [list "x2"] [list "x3"]]
-                 [list "s2" [list "y1"] [list "y2" [list "z1"][list "z2"]] [list "y3"]]]]
 
 
 [define [walk-header-tree t [p 0]]
@@ -182,5 +132,47 @@
   [let [[n [wht-h [car l] r lev i d p]]]
     [if [null? [cdr l]] n [wht-l [cdr l] n lev [+ i [leaf-count [car l]]] d p]]]]
 
+[define [mk-table-header-block t]
+  [stream<-list
+    [map [lambda [x] [cons [car x] [cons "t2" [map number->string [cdr x]]]]]
+         [walk-header-tree t 0]]]]
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+[define test-rs [stream-map [lambda [x] [pad x 2]] [stream-range 21 30]]]
+[define test-cs [stream-map [lambda [x] [pad x 2]] [stream-range 70 81]]]
+[define test-header
+  [list "a"
+    [list [list "s1" [html:br [list] [list]] "new line"] [list "x1"] [list "x2"] [list "x3"]]
+    [list "s2" [list "y1"] [list "y2" [list "z1"][list "z2"]] [list "y3"]]]]
+
+[define [test-df p x y xh yh] [list [cat "h" xh "v" yh "?"] p x "1" y "1"]]
+
+
+[define [html-table-test1]                                            
+  [let* [[ts1 [mk-table-stream-block test-df "t1" 1 test-rs 1 test-cs]]
+         [ts2 [mk-table-header-block test-header]]
+         [name "html-table-test1"]
+         [out-file-html [open-output-file [cat "../" name ".html"] #:exists 'replace]]]
+    [disp-stream ts1]
+    [disp-stream ts2]
+    [write-html
+      [mk-html [list] [list]
+        [list [mk-css-link [cat "./" name ".css"]]]
+        [list]
+        [list
+          [list [html:p [list] [list
+                [html:span [list [list 'class "diff-mod"]] [list "T"]]
+                "est"
+                [html:span [list [list 'class "diff-ins"]] [list "ing"]]]]]
+          [mk-table-divs ts1]
+          [list [html:p [list] [list "New table"]]]
+          [mk-table-divs ts2]
+          ]]
+      out-file-html]
+    [close-output-port out-file-html]
+    [file<-stream [cat "../" name ".css"] displayln
+      [stream-append diff-css table-div-css [mk-table-css [stream ts1 ts2]]]]
+    ]]
 
 [html-table-test1]
